a20a52388c77481f36d45e95147e859fe809caf7
Romy Maghsoudi <romy_mag@outlook.com>
2019-01-13 18:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kitten.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kitten.shift();
+  return kittens
+}
 // Add your functions and code here
